1407ea18537b7f0a74d0543b6c5475c6bc55e7bf
ICEon <matrix_eon@yahoo.com>
2015-02-06 20:22:56 -0600

    create

--- js/acciones.js
+++ js/acciones.js
@@ -3,8 +3,11 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+		//alert ("ready");
 
-	$(  document).on("pageinit", function(event) {
+		
+	$(  document).on("pagecreate", function(event) {
+	//	alert ("dentro");
                   $("#popupNombre", $(this)).popup("open");
                 });
     // Now safe to use device APIs
