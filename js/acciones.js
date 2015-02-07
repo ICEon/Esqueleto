35dee718af8032f9e9ee17ae00924f3fc8a123ba
ICEon <matrix_eon@yahoo.com>
2015-02-07 07:35:11 -0600

    create2

--- js/acciones.js
+++ js/acciones.js
@@ -1,18 +1,19 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady() {
-		alert ("ready");
+document.addEventListener("deviceready", function() {
+
+
+
 
 		
-	$(document).on("pagecreate", function(event) {
-		//alert ("dentro");
+	$(document).on("pagecreate","#principal",function(event){
+		alert ("dentro");
                   $("#popupNombre", $(this)).popup("open");
 			//	  alert ("fuera");
                 });
     // Now safe to use device APIs
 	
 	
-}
+});
 });
