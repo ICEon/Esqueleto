a9d2e78c6787b96feef45f18a1634ec0d31aae22
ICEon <matrix_eon@yahoo.com>
2015-02-07 08:32:29 -0600

    cuatro23

--- js/acciones.js
+++ js/acciones.js
@@ -1,23 +1,16 @@
 // JavaScript Document
 $(document).ready(function(e) {
-$(document).on("pageshow","#principal",function(event){
+$(document).on('pageshow','#principal',function(event){
 		alert ("dos2");
 	});
 document.addEventListener("deviceready", function() {
 alert ("ready");
-
+$(document).on('pageshow','#principal',function(event){
+		alert ("tres3");
+	});
 	
 
-$( '#principal' ).live( 'pagecreate',function(event){
-alert ("alive");
-});
-		
-	$(document).on("pageshow","#principal",function(event){
-		alert ("dentro");
-                 // $("#popupNombre", $(this)).popup("open");
-			//	  alert ("fuera");
-                });
-    // Now safe to use device APIs
+
 	
 	
 });
