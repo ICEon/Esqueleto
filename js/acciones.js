ccac0a6458baca45dfb5fee2a77f19b44f9d57c3
ICEon <matrix_eon@yahoo.com>
2015-02-07 08:50:32 -0600

    mas in

--- js/acciones.js
+++ js/acciones.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 $(document).ready(function(e) {
-	$(document).on('pagebeforecreate','#principal',function(e){
-		alert ("tres3");
-	});
+	
 	
 document.addEventListener( 'deviceready', function() {
 //alert ("ready");
 
-	
+	$(document).on('pagebeforecreate','#principal',function(e){
+		alert ("tres3");
+	});
 	
 });
 });
