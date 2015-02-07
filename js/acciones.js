f0d23c1026af56607e29b54ae2f69e8abed93d8c
ICEon <matrix_eon@yahoo.com>
2015-02-07 07:59:49 -0600

    2dos

--- js/acciones.js
+++ js/acciones.js
@@ -3,13 +3,15 @@ $(document).ready(function(e) {
 
 document.addEventListener("deviceready", function() {
 
-
+	$(document).on("pageshow",function(event){
+		alert ("dos2");
+	});
 
 
 		
 	$(document).on("pageshow","#principal",function(event){
 		alert ("dentro");
-                  $("#popupNombre", $(this)).popup("open");
+                 // $("#popupNombre", $(this)).popup("open");
 			//	  alert ("fuera");
                 });
     // Now safe to use device APIs
